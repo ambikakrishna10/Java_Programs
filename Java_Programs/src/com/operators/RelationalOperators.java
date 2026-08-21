fa0670c0ce69a5510878f804fac8e5992c9d6aee
Ambika Krishna <ambikakrishna2004@gmail.com>
2026-08-21 15:16:13 +0530

    Operators in java

---
+++ Java_Programs/src/com/operators/RelationalOperators.java
@@ -0,0 +1,19 @@
+package com.operators;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+        
+        System.out.println("a value = " + a + " and b value = " + b);
+        
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+    }
+}
