deae54cb5d265a6f1cae0e2c41e375ba468f9175
Ambika Krishna <ambikakrishna2004@gmail.com>
2026-09-02 20:51:35 +0530

    Checking If an array is sorted or not.

--- Java_Programs/src/com/easyPrograms/SortedArrayCheck.java
+++ Java_Programs/src/com/easyPrograms/SortedArrayCheck.java
@@ -2,6 +2,7 @@ package com.easyPrograms;
 
 public class SortedArrayCheck {
 	public static boolean isSorted(int[] arr) {
+		
 		for (int i = 0; i < arr.length - 1; i++) {
 			if (arr[i] > arr[i + 1]) {
 				return false;
@@ -11,6 +12,7 @@ public class SortedArrayCheck {
 	}
 
 	public static void main(String[] args) {
+		
 		int[] arr = { 1, 2, 2, 4, 5, 9 };
 		System.out.println(isSorted(arr));
 	}
