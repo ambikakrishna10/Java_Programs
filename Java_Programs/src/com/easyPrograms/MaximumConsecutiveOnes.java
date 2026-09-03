0225dd067cc8679aa994388195fe76c5d51506ee
Ambika Krishna <ambikakrishna2004@gmail.com>
2026-09-03 14:56:03 +0530

    Maximum consecutive one's program

---
+++ Java_Programs/src/com/easyPrograms/MaximumConsecutiveOnes.java
@@ -0,0 +1,23 @@
+package com.easyPrograms;
+
+public class MaximumConsecutiveOnes {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 1, 0, 1, 1, 1, 0, 1, 1 };
+
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == 1) {
+				count++;
+				if(count > max) {
+					max = count;
+				}
+			}else {
+				count = 0;
+			}
+		}
+		System.out.println("Maximum consecutive ones are: " + max);
+	}
+
+}
